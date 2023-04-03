7325634f345a9ddc7b8c466fdd82f6143d329b9a
Faisal Mansuri <129721028+mansurifaisal@users.noreply.github.com>
2023-04-03 12:57:02 +0530

    Update nested.py

--- nested.py
+++ nested.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
-    tag: list = []
+    tag: set[str] = set()
+    image: list[Image] | None = None
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item:Item):
